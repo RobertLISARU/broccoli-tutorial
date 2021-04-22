653f300434fb5b9799d69e25c3e768b7136f1afe
Lisaru Robert <lisaru.robert@gmail.com>
2021-04-22 10:11:43 +0300

    Funnel and merge files

--- Brocfile.js
+++ Brocfile.js
@@ -1,1 +1,29 @@
-module.exports = "app"
+const funnel = require("broccoli-funnel")
+const merge = require("broccoli-merge-trees")
+
+const appRoot = "app"
+
+const html = funnel(appRoot, {
+    files: ["index.html"],
+    destDir: "/",
+    annotation: 'Index file',
+})
+
+const js = funnel(appRoot, {
+    files: ["app.js"],
+    destDir: "/assets",
+    annotation: "JS files"
+})
+
+const css = funnel(appRoot, {
+    srcDir: "styles",
+    files: ["app.css"],
+    destDir: "/assets",
+    annotation: "CSS files"
+})
+
+const public = funnel('public', {
+    annotation: "Public files"
+})
+
+module.exports = merge([html, js, css, public], { annotation: "Final output" })
